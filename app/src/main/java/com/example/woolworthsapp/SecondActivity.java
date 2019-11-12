a0402402812ed6bc17f375fe67e9bbd9650e718f
jc319816 <jnegr3@gmail.com>
2019-11-12 13:31:24 +1000

    Creating the mutiple activities

--- app/src/main/java/com/example/woolworthsapp/SecondActivity.java
+++ app/src/main/java/com/example/woolworthsapp/SecondActivity.java
@@ -1,14 +1,62 @@
 package com.example.woolworthsapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        setupUIViews();
+        initToolbar();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupUIViews() {
+        toolbar = (Toolbar)findViewById(R.id.ToolbarMain);
+        listView = (ListView)findViewById(R.id.lvMain);
+    }
+
+    private void initToolbar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Product App");
+
+    }
+
+    public class SimpleAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
